ce895441cbaa68a8ce6972884cf54a46b89a2ace
LCOSTA110280 <LEOCOSRA110280@gMAIL.COM>
2023-01-08 17:34:18 -0300

    04 App

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { Text, View } from 'react-native';
 
-export default function App() {
+class App extends Component {
+  render(){  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1,backgroundColor:'gray'}}>
+      <View style={{flex:1,backgroundColor:'red'}}>
+      </View>
+      <View style={{flex:1,backgroundColor:'yellow'}}>
+      </View>
+      <View style={{flex:2,backgroundColor:'green'}}>
+      </View>
     </View>
   );
 }
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
+
